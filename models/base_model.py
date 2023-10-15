e0f0ee532625fb8a3dba83df17705392d9ad34ec
Mohamed AJJAJI <majjaji98@gmail.com>
2023-10-15 08:34:44 -0700

    update base_model

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Module: base_model.py """
 import uuid
+import models
 from datetime import datetime
 
 
@@ -8,11 +9,20 @@ class BaseModel():
     """ Base class which defines all common attributes/methods
     for other classes """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ Instantiate an object with its attributes """
+        fmt = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        if len(kwargs) != 0:
+            for i, j in kwargs.items():
+                if i == "created_at" or i == "updated_at":
+                    self.__dict__[i] = datetime.strptime(j, fmt)
+                else:
+                    self.__dict__[i] = j
+        else:
+            models.storage.new(self)
 
     def __str__(self):
         """ Returns the string representation of the instance """
@@ -23,6 +33,7 @@ class BaseModel():
         """ Updates the public instance attribute updated_at
         with the current datetime """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """ Returns a dictionary containing all keys/values
