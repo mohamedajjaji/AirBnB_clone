a4ec6aeb396f54e3d477816b817391c00220d43a
Mohamed AJJAJI <majjaji98@gmail.com>
2023-10-15 15:16:42 -0700

    fixed errors

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Module: base_model.py """
-import uuid
+import uuid import uuid4
 import models
 from datetime import datetime
 
@@ -26,8 +26,8 @@ class BaseModel():
 
     def __str__(self):
         """ Returns the string representation of the instance """
-        s = "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
-        return s
+        s = self.__class__.__name__
+        return "[{}] ({}) {}".format(s, self.id, self.__dict__)
 
     def save(self):
         """ Updates the public instance attribute updated_at
@@ -38,8 +38,8 @@ class BaseModel():
     def to_dict(self):
         """ Returns a dictionary containing all keys/values
         of __dict__ of the instance """
-        obj_dict = {self.__dict__}
-        obj_dict['__class__'] = type(self).__name__
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = obj_dict['created_at'].isoformat()
         obj_dict['updated_at'] = obj_dict['updated_at'].isoformat()
 
