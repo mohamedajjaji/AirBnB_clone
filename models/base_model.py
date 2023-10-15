8d4413240a4d5e3c7f7bd66d28b570543bd2f86d
Mohamed AJJAJI <majjaji98@gmail.com>
2023-10-15 15:45:32 -0700

    Fixed an error in __init__ define

--- models/base_model.py
+++ models/base_model.py
@@ -12,7 +12,7 @@ class BaseModel():
     def __init__(self, *args, **kwargs):
         """ Instantiate an object with its attributes """
         fmt = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid.uuid4())
+        self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
         if len(kwargs) != 0:
